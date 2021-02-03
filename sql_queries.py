69c72d397fda402b26436fe5201915f04ccf5b35
donjude <donjude83@live.com>
2021-02-03 22:26:47 +0000

    project update

--- sql_queries.py
+++ sql_queries.py
@@ -1,59 +1,122 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP TABLE IF EXISTS fact_songplay"
-user_table_drop = "DROP TABLE IF EXISTS dim_user"
-song_table_drop = "DROP TABLE IF EXISTS dim_song"
-artist_table_drop = "DROP TABLE IF EXISTS dim_artist"
-time_table_drop = "DROP TABLE IF EXISTS dim_time"
+songplay_table_drop = "DROP TABLE IF EXISTS songplay"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""CREATE TABLE IF NOT EXISTS fact_songplay (
-    songplay_id int, start_time time, user_id int, level varchar, song_id, artist_id, session_id, location, user_agent
-    )
+songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays
+(
+    songplay_id SERIAL PRIMARY KEY
+    ,start_time time
+    ,user_id int
+    ,level varchar(50)
+    ,song_id varchar(150)
+    ,artist_id varchar(150)
+    ,session_id int
+    ,location varchar(150)
+    ,user_agent text
+)
 """)
 
-user_table_create = ("""CREATE TABLE IF NOT EXISTS dim_user (
-    user_id int, first_name varchar, last_name varchar, gender varchar, level varchar
-    )
+user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users
+(
+    user_id int PRIMARY KEY
+    ,first_name varchar(150)
+    ,last_name varchar(150)
+    ,gender varchar(50)
+    ,level varchar(50)
+)
 """)
 
-song_table_create = ("""CREATE TABLE IF NOT EXISTS dim_song (
-    song_id int, title varchar, artist_id, year int, duration numeric(18,5)
-    )
+song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs
+(
+    song_id varchar(150) PRIMARY KEY
+    ,title varchar(150)
+    ,artist_id varchar(150)
+    ,year int
+    ,duration numeric(18,6)
+)
 """)
 
-artist_table_create = ("""CREATE TABLE IF NOT EXISTS dim_artist (
-    artist_id int, name varchar, location varchar, latitude numeric(10,6), longitude numeric(10,6)
-    )
+artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists
+(
+    artist_id varchar(150) PRIMARY KEY
+    ,name text
+    ,location varchar(150)
+    ,latitude numeric(10,6)
+    ,longitude numeric(10,6)
+)
 """)
 
-time_table_create = ("""CREATE TABLE IF NOT EXISTS dim_time (
-    start_time, hour, day, week, month, year, weekday
+time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time
+(
+    start_time time PRIMARY KEY
+    ,hour int
+    ,day int
+    ,week int
+    ,month int
+    ,year int
+    ,weekday varchar(50)
     )
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+    INSERT INTO songplays
+    (songplay_id ,start_time ,user_id ,level ,song_id ,artist_id ,session_id ,location ,user_agent)
+    VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s);
 """)
 
 user_table_insert = ("""
+    INSERT INTO users
+    (user_id, first_name, last_name, gender, level)
+    VALUES(%s, %s, %s, %s, %s)
+    ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level;
 """)
 
 song_table_insert = ("""
+    INSERT INTO songs
+    (song_id, title, artist_id, year, duration)
+    VALUES(%s, %s, %s, %s, %s)
+    ON CONFLICT (song_id) DO NOTHING;
 """)
 
 artist_table_insert = ("""
+    INSERT INTO artists
+    (artist_id, name, location, latitude, longitude)
+    VALUES(%s, %s, %s, %s, %s)
+    ON CONFLICT (artist_id) DO NOTHING;
 """)
 
 
 time_table_insert = ("""
+    INSERT INTO time
+    (start_time, hour, day, week, month, year, weekday)
+    VALUES(%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (start_time) DO NOTHING;
 """)
 
 # FIND SONGS
 
 song_select = ("""
+    SELECT songs.song_id
+        ,songs.title
+        ,artists.name
+    FROM songs JOIN artists
+    ON songs.artist_id = artists.artist_id
+    WHERE songs.title = %s
+    AND artists.name = %s
+    AND songs.duration = %s
 """)
 
 # QUERY LISTS
