ae2d33a60821ff6489a54c68f299f47bd7cf3a41
donjude <donjude83@live.com>
2021-02-04 16:05:33 +0000

    update scripts

--- etl.py
+++ etl.py
@@ -7,37 +7,50 @@ from sql_queries import *
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = 
+    columns = df[['song_id', 'title', 'artist_id', 'year', 'duration']].columns.tolist()
+    song_data = df.loc[:,columns].values.tolist()
+    
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    art_cols = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].columns.tolist()
+    artist_data = df.loc[:,art_cols].values.tolist()
+    
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df['page'] == 'NextSong']
 
     # convert timestamp column to datetime
-    t = 
+    t = pd.to_datetime(df['ts'], unit='ms')
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    timestamp = t
+    hour = t.dt.hour
+    day = t.dt.day
+    week = t.dt.week
+    month = t.dt.month
+    year = t.dt.year
+    weekday = t.dt.day_name()
+    
+    
+    time_data = ([timestamp, hour, day, week, month, year, weekday])
+    column_labels = ('start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday')
+    time_df = pd.DataFrame(dict(zip(column_labels, time_data)))
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
 
     # insert user records
     for i, row in user_df.iterrows():
--- sql_queries-Copy1.py
+++ sql_queries-Copy1.py
@@ -1,6 +1,6 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+songplay_table_drop = "DROP TABLE IF EXISTS songplay"
 user_table_drop = "DROP TABLE IF EXISTS users"
 song_table_drop = "DROP TABLE IF EXISTS songs"
 artist_table_drop = "DROP TABLE IF EXISTS artists"
@@ -11,14 +11,14 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS songplays
 (
-    songplay_id SERIAL PRIMARY KEY 
-    ,start_time TIMESTAMP REFERENCES time(start_time)
-    ,user_id int NOT NULL REFERENCES users(user_id)
-    ,level text
-    ,song_id text REFERENCES songs(song_id)
-    ,artist_id text REFERENCES artists(artist_id)
+    songplay_id SERIAL PRIMARY KEY
+    ,start_time timestamp
+    ,user_id int
+    ,level varchar(50)
+    ,song_id varchar(200)
+    ,artist_id varchar(200)
     ,session_id int
-    ,location text
+    ,location varchar(200)
     ,user_agent text
 )
 """)
@@ -27,19 +27,19 @@ user_table_create = ("""
 CREATE TABLE IF NOT EXISTS users
 (
     user_id int PRIMARY KEY
-    ,first_name text NOT NULL 
-    ,last_name text NOT NULL
-    ,gender text
-    ,level text
+    ,first_name varchar(150)
+    ,last_name varchar(150)
+    ,gender varchar(50)
+    ,level varchar(50)
 )
 """)
 
 song_table_create = ("""
 CREATE TABLE IF NOT EXISTS songs
 (
-    song_id text PRIMARY KEY
+    song_id varchar(200) PRIMARY KEY
     ,title text NOT NULL
-    ,artist_id text NOT NULL
+    ,artist_id varchar(200) NOT NULL
     ,year int
     ,duration float NOT NULL
 )
@@ -48,53 +48,53 @@ CREATE TABLE IF NOT EXISTS songs
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS artists
 (
-    artist_id text PRIMARY KEY
-    ,name text NOT NULL
-    ,location text 
-    ,lattitude float 
-    ,longitude float
+    artist_id varchar(200) PRIMARY KEY
+    ,name text
+    ,location varchar(200)
+    ,latitude numeric(10,6)
+    ,longitude numeric(10,6)
 )
 """)
 
 time_table_create = ("""
 CREATE TABLE IF NOT EXISTS time
 (
-    start_time TIMESTAMP PRIMARY KEY
+    start_time timestamp PRIMARY KEY
     ,hour int
     ,day int
     ,week int
     ,month int
     ,year int
-    ,weekday text
-)
+    ,weekday varchar(50)
+    )
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
     INSERT INTO songplays
-    (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+    (songplay_id ,start_time ,user_id ,level ,song_id ,artist_id ,session_id ,location ,user_agent)
+    VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s);
 """)
 
 user_table_insert = ("""
     INSERT INTO users
     (user_id, first_name, last_name, gender, level)
-    VALUES (%s, %s, %s, %s, %s)
+    VALUES(%s, %s, %s, %s, %s)
     ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level;
 """)
 
 song_table_insert = ("""
     INSERT INTO songs
     (song_id, title, artist_id, year, duration)
-    VALUES (%s, %s, %s, %s, %s)
+    VALUES(%s, %s, %s, %s, %s)
     ON CONFLICT (song_id) DO NOTHING;
 """)
 
 artist_table_insert = ("""
     INSERT INTO artists
-    (artist_id, name, location, lattitude, longitude)
-    VALUES (%s, %s, %s, %s, %s)
+    (artist_id, name, location, latitude, longitude)
+    VALUES(%s, %s, %s, %s, %s)
     ON CONFLICT (artist_id) DO NOTHING;
 """)
 
@@ -102,7 +102,7 @@ artist_table_insert = ("""
 time_table_insert = ("""
     INSERT INTO time
     (start_time, hour, day, week, month, year, weekday)
-    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    VALUES(%s, %s, %s, %s, %s, %s, %s)
     ON CONFLICT (start_time) DO NOTHING;
 """)
 
@@ -111,12 +111,11 @@ time_table_insert = ("""
 song_select = ("""
     SELECT song_id, artists.artist_id
     FROM songs JOIN artists ON songs.artist_id = artists.artist_id
-    WHERE songs.title = %s
-    AND artists.name = %s
+    WHERE artists.name = %s
     AND songs.duration = %s
 """)
 
 # QUERY LISTS
 
-create_table_queries = [user_table_create, artist_table_create, song_table_create, time_table_create, songplay_table_create]
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
